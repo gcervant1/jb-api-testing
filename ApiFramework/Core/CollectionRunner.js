219394781434ecc3fd89e355748edb8a135e9c52
Gonzalo Cervantes <gonzalocervantes@Gonzalos-MacBook-Pro.local>
2017-01-25 21:10:09 -0600

    test

--- ApiFramework/Core/CollectionRunner.js
+++ ApiFramework/Core/CollectionRunner.js
@@ -1,18 +1,13 @@
 
 var newman = require('newman')
-var filesystem = require('fs');
-// call newman.run to pass `options` object and wait for callback
-if(fs.existsSync('../Collections/CollectionTest.json')) {
-    // Do something
-    newman.run({
-        collection: require(process.cwd()+'/ApiFramework/Collections/CollectionTest.json'),
-        reporters: 'cli'
-    }, function (err) {
-        if (err) { throw err; }
-        console.info('collection run complete!');
-    });
-}
-else
-{
-  console.log('Collection was not found.' + process.cwd() +);
-}
+
+// Do something
+newman.run({
+    collection: require(process.cwd().toString() + '/ApiFramework/Collections/CollectionTest.json'),
+    reporters: 'cli'
+}, function(err) {
+    if (err) {
+        throw err;
+    }
+    console.info('collection run complete!');
+});
